fbe7360f99c194cca235db3ab733035f5ab1dbf8
sly <sylvaincadoret@hotmail.com>
2024-12-19 10:01:42 +0100

    fetch all expenses ok

--- src/pages/projectDetails/projectDetails.jsx
+++ src/pages/projectDetails/projectDetails.jsx
@@ -40,12 +40,12 @@ export default function ProjectDetails() {
         setProject(projectData);
         setCompany(companyData);
         // Vérification si expensesData est un tableau
-        if (Array.isArray(expensesData)) {
+        if (expensesData && Array.isArray(expensesData)) {
           setExpenses(expensesData);
         } else {
-          setExpenses([]); // Si ce n'est pas un tableau, initialiser comme un tableau vide
-          setError("Les données des dépenses sont incorrectes.");
+          setError("Format inattendu des données des dépenses.");
         }
+        
       } catch (err) {
         console.error(err);
         if (err.message.includes("expenses")) {
@@ -62,66 +62,9 @@ export default function ProjectDetails() {
   
     loadProjectData();
   }, [projectId]);
+
+
   
-  // Affichage des dépenses avec une vérification préalable
-  return (
-    <div className={styles.expensesContainer}>
-      <h1>Dépenses</h1>
-  
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Titre</th>
-            <th>Date</th>
-            <th>Montant HT</th>
-            <th>Montant TTC</th>
-            <th>Reste à payer</th>
-            <th>Code comptable</th>
-            <th>Code comptable nom</th>
-            <th>Contrat fournisseur</th>
-            <th>Fournisseur</th>
-            <th>Entreprise</th>
-            <th>Projet</th>
-            <th>Public path</th>
-          </tr>
-        </thead>
-  
-        <tbody>
-          {Array.isArray(expenses) && expenses.length > 0 ? (
-            expenses.map((expense) => (
-              <tr key={expense.id}>
-                <td>{expense.id}</td>
-                <td>{expense.title}</td>
-                <td>{new Date(expense.date).toLocaleDateString()}</td>
-                <td>{expense.pre_tax_amount.toFixed(2)} €</td>
-                <td>{expense.total_amount.toFixed(2)} €</td>
-                <td>{expense.left_to_pay.toFixed(2)} €</td>
-                <td>{expense.accounting_code}</td>
-                <td>{expense.accounting_code_name}</td>
-                <td>{expense.supplier_contract_id}</td>
-                <td>{expense.supplier_name}</td>
-                <td>{expense.company_id}</td>
-                <td>{expense.project_id}</td>
-                <td>{expense.public_path}</td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="13">Aucune dépense disponible.</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
-  );
-  
-
-
-
-
-
-  console.log (expenses);
   if (loading) {
     return (
       <div className={styles.loaderContainer}>
@@ -146,107 +89,111 @@ export default function ProjectDetails() {
     },
     { name: "Différence (Revenu - Dépenses)", difference },
   ];
-
+  
+  // Affichage des dépenses avec une vérification préalable
   return (
+
     <div className={styles.projectContainer}>
-      <h1>Détails du projet - {project.number}</h1>
+     <h1>Détails du projet - {project.number}</h1>
 
-      <div className={styles.header}>
-        <div className={styles.section1}>
-          <p>
-            <strong>Id :</strong> {project.id}
-          </p>
-          <p>
-            <strong>Numéro :</strong> {project.number}
-          </p>
-          <p>
-            <strong>Nom :</strong> {project.name}
-          </p>
-        </div>
-        <div className={styles.section2}>
-          <p>
-            <strong>Entreprise :</strong>{" "}
-            {company.name || "Entreprise inconnue"}
-          </p>
-          <p>
-            <strong>Adresse :</strong> {company.address_street}
-          </p>
-          <p>
-            <strong>Code postal :</strong> {company.zip_code}
-          </p>
-          <p>
-            <strong>Ville :</strong> {company.address_city} (
-            {company.address_country})
-          </p>
-          <p>
-            <strong>Commercial en charge :</strong>{" "}
-            {company.business_manager?.name || "Inconnu"}
-          </p>
-        </div>
-      </div>
+<div className={styles.header}>
+  <div className={styles.section1}>
+    <p>
+      <strong>Id :</strong> {project.id}
+    </p>
+    <p>
+      <strong>Numéro :</strong> {project.number}
+    </p>
+    <p>
+      <strong>Nom :</strong> {project.name}
+    </p>
+  </div>
+  <div className={styles.section2}>
+    <p>
+      <strong>Entreprise :</strong>{" "}
+      {company.name || "Entreprise inconnue"}
+    </p>
+    <p>
+      <strong>Adresse :</strong> {company.address_street}
+    </p>
+    <p>
+      <strong>Code postal :</strong> {company.zip_code}
+    </p>
+    <p>
+      <strong>Ville :</strong> {company.address_city} (
+      {company.address_country})
+    </p>
+    <p>
+      <strong>Commercial en charge :</strong>{" "}
+      {company.business_manager?.name || "Inconnu"}
+    </p>
+  </div>
+</div>
 
-      {/* Section Graphique */}
-      <div className={styles.graphContainer}>
-        <h2>Graphique des coûts et revenus</h2>
-        <ResponsiveContainer width="100%" height={400}>
-          <BarChart
-            data={chartData}
-            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-          >
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="estimatedRevenue" fill="#4682B4" />
-            <Bar dataKey="actualRevenue" fill="#2E8B57" />
-            <Bar dataKey="actualExpenses" fill="#FF69B4" />
-            <Bar dataKey="difference" fill="#FFD700" />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
+{/* Section Graphique */}
+<div className={styles.graphContainer}>
+  <h2>Graphique des coûts et revenus</h2>
+  <ResponsiveContainer width="100%" height={400}>
+    <BarChart
+      data={chartData}
+      margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+    >
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Bar dataKey="estimatedRevenue" fill="#4682B4" />
+      <Bar dataKey="actualRevenue" fill="#2E8B57" />
+      <Bar dataKey="actualExpenses" fill="#FF69B4" />
+      <Bar dataKey="difference" fill="#FFD700" />
+    </BarChart>
+  </ResponsiveContainer>
+</div>
 
-      <div className={styles.expensesContainer}>
-        <h1>Dépenses</h1>
+<div className={styles.expensesContainer}>
+  <h1>Dépenses</h1>
 
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Titre</th>
-              <th>Date</th>
-              <th>Montant HT</th>
-              <th>Montant TTC</th>
-              <th>Reste à payer</th>
-              <th>Code comptable</th>
-              <th>Code comptable nom</th>
-              <th>Contrat fournisseur</th>
-              <th>Fournisseur</th>
-              <th>Entreprise</th>
-              <th>Projet</th>
-              <th>Public path</th>
-            </tr>
-          </thead>
+  <table>
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Titre</th>
+        <th>Date</th>
+        <th>Montant HT</th>
+        <th>Montant TTC</th>
+        <th>Reste à payer</th>
+        <th>Code comptable</th>
+        <th>Code comptable nom</th>
+        <th>Contrat fournisseur</th>
+        <th>Fournisseur</th>
+        <th>Entreprise</th>
+        <th>Projet</th>
+        <th>Public path</th>
+      </tr>
+    </thead>
 
-          <tbody>
-            {expenses.map((expense) => (
-              <tr key={expense.id}>
-                <td>{expense.id}</td>
-                <td>{expense.title}</td>
-                <td>{new Date(expense.date).toLocaleDateString()}</td>
-                <td>{expense.pre_tax_amount.toFixed(2)} €</td>
-                <td>{expense.total_amount.toFixed(2)} €</td>
-                <td>{expense.left_to_pay.toFixed(2)} €</td>
-                <td>{expense.accounting_code}</td>
-                <td>{expense.accounting_code_name}</td>
-                <td>{expense.supplier_contract_id}</td>
-                <td>{expense.supplier_name}</td>
-                <td>{expense.company_id}</td>
-                <td>{expense.project_id}</td>
-                <td>{expense.public_path}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    <tbody>
+      {expenses.map((expense) => (
+        <tr key={expense.id}>
+          <td>{expense.id}</td>
+          <td>{expense.title}</td>
+          <td>{new Date(expense.date).toLocaleDateString()}</td>
+          <td>{expense.pre_tax_amount.toFixed(2)} €</td>
+          <td>{expense.total_amount.toFixed(2)} €</td>
+          <td>{expense.left_to_pay.toFixed(2)} €</td>
+          <td>{expense.accounting_code}</td>
+          <td>{expense.accounting_code_name}</td>
+          <td>{expense.supplier_contract_id}</td>
+          <td>{expense.supplier_name}</td>
+          <td>{expense.company_id}</td>
+          <td>{expense.project_id}</td>
+          <td>{expense.public_path}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+</div>
+);
 }
+
+    
--- src/services/api/expenses.js
+++ src/services/api/expenses.js
@@ -1,20 +1,45 @@
-export const fetchExpensesByProject = async (estimatedStart, estimatedEnd, page = 1) => {
-    const response = await fetch(
-      `http://localhost:3001/expensesByProject?estimatedStart=${encodeURIComponent(
-        estimatedStart
-      )}&estimatedEnd=${encodeURIComponent(estimatedEnd)}&page=${page}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+// export const fetchExpensesByProject = async (estimatedStart, estimatedEnd, page = 1) => {
+//     const response = await fetch(
+//       `http://localhost:3001/expensesByProject?estimatedStart=${encodeURIComponent(
+//         estimatedStart
+//       )}&estimatedEnd=${encodeURIComponent(estimatedEnd)}&page=${page}`,
+//       {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
     
-    if (!response.ok) {
-      throw new Error("Erreur lors de la récupération des dépenses.");
-    }
+//     if (!response.ok) {
+//       throw new Error("Erreur lors de la récupération des dépenses.");
+//     }
     
-    return response.json();
-  };
+//     // return response.json();
+//     return response.json().then((data) => data.expenses);
+
+//   };
   
+
+export const fetchExpensesByProject = async (estimatedStart, estimatedEnd, page = 1) => {
+  const response = await fetch(
+    `http://localhost:3001/expensesByProject?estimatedStart=${encodeURIComponent(
+      estimatedStart
+    )}&estimatedEnd=${encodeURIComponent(estimatedEnd)}&page=${page}`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Erreur lors de la récupération des dépenses.");
+  }
+
+  const data = await response.json();
+  if (!Array.isArray(data.expenses)) {
+    throw new Error("Format inattendu des données des dépenses.");
+  }
+
+  return data.expenses;
+};
